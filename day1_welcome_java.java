c65ee8baa71afab2d82e733edad29f5d3e3bfd88
Marwanmarw <m8315121@gmail.com>
2022-02-04 11:09:02 +0800

    Day 3

--- day1_welcome_java.java
+++ day1_welcome_java.java
@@ -1,4 +1,4 @@
-public class Day1_welcome_java {
+public class day1_welcome_java {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");     
